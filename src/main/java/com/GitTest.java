a47c3bf25c508e8200f6d30f00da91bd111fd2bf
Runoob <Runoob@123.com>
2022-01-09 19:29:30 +0800

    Merge branch 'hot-fix'
    # Conflicts:
    #	src/main/java/com/GitTest.java

--- src/main/java/com/GitTest.java
+++ src/main/java/com/GitTest.java
@@ -8,5 +8,7 @@ public class GitTest {
         System.out.println("hello");
         System.out.println("master");
         System.out.println("hot-fix");
+        System.out.println("push");
+        System.out.println("push");
     }
 }
